bab5e74e575a1b85437e4cbe4b19524a1bbf675f
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-19 21:51:17 -0400

    imported styled from styled-components in SignUpClient.js

--- src/components/SignupClient.js
+++ src/components/SignupClient.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import * as yup from "yup"
-import schema from '../formValidation/signUpSchema'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import * as yup from "yup";
+import schema from '../formValidation/signUpSchema';
+import styled from 'styled-components';
 
 const clientInitialvalues = {
     name: '',
