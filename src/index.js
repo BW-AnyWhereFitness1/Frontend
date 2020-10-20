8e5218e76b9f8206a5dc02f775d0230c7710c189
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-19 21:49:00 -0400

    Added themeprovider to index.js and created database of frequently used stylings in src/theme.index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme'
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'
+import rootReducer from './reducers';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
+    <ThemeProvider theme={theme}>
         <App />
+    </ThemeProvider>
     </Provider>, document.getElementById('root'));
 
